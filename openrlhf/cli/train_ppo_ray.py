07885b1823de3186431166463515047d0f287514
Brayan Jaramillo <bryn07@gmail.com>
2025-10-08 19:24:03 -0400

    Trigger build

--- openrlhf/cli/train_ppo_ray.py
+++ openrlhf/cli/train_ppo_ray.py
@@ -433,7 +433,7 @@ def get_parser():
         help="How often the SubmissionActor should update submission.json (in seconds).",
     )
 
-    # Checkpoints
+    # Checkpoints:
     parser.add_argument("--eval_steps", type=int, default=-1)
     parser.add_argument("--save_steps", type=int, default=-1)
     parser.add_argument("--logging_steps", type=int, default=1)
